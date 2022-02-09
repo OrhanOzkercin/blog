77a55a4a7453a3bf40daf7713f735cf583973b70
Orhan Özkerçin <orhanozkercin@yahoo.com>
2022-02-10 01:11:19 +0300

    feature: add ability to get posts from cms

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,9 @@
 import Head from 'next/head'
 import { Categories, PostCard, PostWidget } from '../components'
+import { Post } from '../models/Post.model'
+import { getPosts } from '../services'
 
-export default function Home() {
-  const posts = [
-    { title: 'React Testing', excerpt: 'Learn React Testing' },
-    {
-      title: 'React Testing w Tailiwnd',
-      excerpt: 'Learn React Testing Tailwind',
-    },
-  ]
+export default function Home({ posts }: { posts: Post[] }) {
   return (
     <div className="container mx-1 mb-8  px-10 ">
       <Head>
@@ -18,7 +13,7 @@ export default function Home() {
       <div className="grid grid-cols-1 gap-12 lg:grid-cols-12">
         <div className="col-span-1 lg:col-span-8">
           {posts.map((post, index) => (
-            <PostCard post={post} key={post.title} />
+            <PostCard post={post} key={post.slug} />
           ))}
         </div>
         <div className="col-span-1 lg:col-span-4">
@@ -30,4 +25,12 @@ export default function Home() {
       </div>
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const posts = (await getPosts()) || []
+  console.log('posts:', posts)
+  return {
+    props: { posts: posts.map(({ node }: any) => node) },
+  }
 }
